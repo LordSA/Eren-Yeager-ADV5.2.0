ca59d9079697e5951619056130f8ef87de07f69d
Shibili Aman TK <dlshacker159@gmail.com>
2025-11-04 22:07:30 +0530

    time complexity fixed for admin commands

--- plugins/admin_tools.py
+++ plugins/admin_tools.py
@@ -1,5 +1,6 @@
 import asyncio
-from time import time, sleep
+import logging
+from time import time
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
@@ -8,30 +9,24 @@ from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired, UserAd
 from plugins.Tools.help_func.admin_check import admin_check
 from plugins.Tools.help_func.cust_p_filters import f_onw_fliter, admin_fliter
 from Script import script
-from info import ADMINS # Imported for the instatus check
+from info import ADMINS
 
-# Max messages to delete in one go
+# Get a logger for this file
+logger = logging.getLogger(__name__)
 TG_MAX_SELECT_LEN = 400
 
-@Client.on_message(
-    filters.command("purge") &
-    f_onw_fliter
-)
+@Client.on_message(filters.command("purge") & f_onw_fliter)
 async def purge(client, message):
     """ purge upto the replied message """
     if message.chat.type not in ("supergroup", "channel"):
         return
-
     is_admin = await admin_check(message)
-
     if not is_admin:
         return
-
     status_message = await message.reply_text("...", quote=True)
     await message.delete()
     message_ids = []
     count_del_etion_s = 0
-
     if message.reply_to_message:
         for a_s_message_id in range(
             message.reply_to_message.message_id,
@@ -61,20 +56,14 @@ async def purge(client, message):
     await status_message.delete()
 
 
-@Client.on_message(
-    filters.command(["pin"]) &
-    admin_fliter
-)
+@Client.on_message(filters.command(["pin"]) & admin_fliter)
 async def pin(_, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.pin()
 
 
-@Client.on_message(
-    filters.command(["unpin"]) &
-    admin_fliter
-)
+@Client.on_message(filters.command(["unpin"]) & admin_fliter)
 async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
@@ -82,80 +71,83 @@ async def unpin(_, message: Message):
 
 
 @Client.on_message(filters.incoming & ~filters.private & filters.command('inkick'))
-def inkick(client, message):
-    user = client.get_chat_member(message.chat.id, message.from_user.id)
+async def inkick(client, message):
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
     if user.status == ("creator"):
         if len(message.command) > 1:
             input_str = message.command
-            sent_message = message.reply_text(script.START_KICK)
-            sleep(20)
-            sent_message.delete()
-            message.delete()
+            sent_message = await message.reply_text(script.START_KICK)
+            await asyncio.sleep(20)
+            await sent_message.delete()
+            await message.delete()
             count = 0
-            for member in client.iter_chat_members(message.chat.id):
+            
+            async for member in client.get_chat_members(message.chat.id): 
                 if member.user.status in input_str and not member.status in ('administrator', 'creator'):
                     try:
-                        client.kick_chat_member(message.chat.id, member.user.id, int(time() + 45))
+                        await client.kick_chat_member(message.chat.id, member.user.id, int(time() + 45))
                         count += 1
-                        sleep(1)
+                        await asyncio.sleep(1)
                     except (ChatAdminRequired, UserAdminInvalid):
-                        sent_message.edit(script.ADMIN_REQUIRED)
-                        client.leave_chat(message.chat.id)
+                        await sent_message.edit(script.ADMIN_REQUIRED)
+                        await client.leave_chat(message.chat.id)
                         break
                     except FloodWait as e:
-                        sleep(e.x)
+                        await asyncio.sleep(e.x)
+                    except Exception as e:
+                        logger.error(f"Failed to kick user {member.user.id}: {e}")
             try:
-                sent_message.edit(script.KICKED.format(count))
+                await sent_message.edit(script.KICKED.format(count))
             except ChatWriteForbidden:
                 pass
         else:
-            message.reply_text(script.INPUT_REQUIRED)
+            await message.reply_text(script.INPUT_REQUIRED)
     else:
-        sent_message = message.reply_text(script.CREATOR_REQUIRED)
-        sleep(5)
-        sent_message.delete()
-        message.delete()
+        sent_message = await message.reply_text(script.CREATOR_REQUIRED)
+        await asyncio.sleep(5)
+        await sent_message.delete()
+        await message.delete()
 
 
 @Client.on_message(filters.incoming & ~filters.private & filters.command('dkick'))
-def dkick(client, message):
-    user = client.get_chat_member(message.chat.id, message.from_user.id)
+async def dkick(client, message):
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
     if user.status == ("creator"):
-        sent_message = message.reply_text(script.START_KICK)
-        sleep(20)
-        sent_message.delete()
-        message.delete()
+        sent_message = await message.reply_text(script.START_KICK)
+        await asyncio.sleep(20)
+        await sent_message.delete()
+        await message.delete()
         count = 0
-        for member in client.iter_chat_members(message.chat.id):
+        async for member in client.get_chat_members(message.chat.id): 
             if member.user.is_deleted and not member.status in ('administrator', 'creator'):
                 try:
-                    client.kick_chat_member(message.chat.id, member.user.id, int(time() + 45))
+                    await client.kick_chat_member(message.chat.id, member.user.id, int(time() + 45))
                     count += 1
-                    sleep(1)
+                    await asyncio.sleep(1)
                 except (ChatAdminRequired, UserAdminInvalid):
-                    sent_message.edit(script.ADMIN_REQUIRED)
-                    client.leave_chat(message.chat.id)
+                    await sent_message.edit(script.ADMIN_REQUIRED)
+                    await client.leave_chat(message.chat.id)
                     break
                 except FloodWait as e:
-                    sleep(e.x)
+                    await asyncio.sleep(e.x)
+                except Exception as e:
+                    logger.error(f"Failed to dkick user {member.user.id}: {e}")
         try:
-            sent_message.edit(script.DKICK.format(count))
+            await sent_message.edit(script.DKICK.format(count))
         except ChatWriteForbidden:
             pass
     else:
-        sent_message = message.reply_text(script.CREATOR_REQUIRED)
-        sleep(5)
-        sent_message.delete()
-        message.delete()
+        sent_message = await message.reply_text(script.CREATOR_REQUIRED)
+        await asyncio.sleep(5)
+        await sent_message.delete()
+        await message.delete()
 
 
 @Client.on_message(filters.incoming & ~filters.private & filters.command('instatus'))
-def instatus(client, message):
-    user = client.get_chat_member(message.chat.id, message.from_user.id)
-    # Note: Checking for 'ADMINS' as a string is likely a bug, 
-    # but I am preserving the original logic.
+async def instatus(client, message):
+    user = await client.get_chat_member(message.chat.id, message.from_user.id)
     if user.status in ('administrator', 'creator', 'ADMINS'): 
-        sent_message = message.reply_text(script.FETCHING_INFO)
+        sent_message = await message.reply_text(script.FETCHING_INFO)
         recently = 0
         within_week = 0
         within_month = 0
@@ -163,7 +155,8 @@ def instatus(client, message):
         deleted_acc = 0
         uncached = 0
         bot = 0
-        for member in client.iter_chat_members(message.chat.id):
+        
+        async for member in client.get_chat_members(message.chat.id):
             user = member.user
             if user.is_deleted:
                 deleted_acc += 1
@@ -179,4 +172,4 @@ def instatus(client, message):
                 long_time_ago += 1
             else:
                 uncached += 1
-        sent_message.edit(script.STATUS.format(message.chat.title, recently, within_week, within_month, long_time_ago, deleted_acc, bot, uncached))
+        await sent_message.edit(script.STATUS.format(message.chat.title, recently, within_week, within_month, long_time_ago, deleted_acc, bot, uncached))
