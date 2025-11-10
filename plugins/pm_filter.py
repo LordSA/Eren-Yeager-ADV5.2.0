e63ed69492eb91227c4c70d34b38a053755f3b26
Shibili Aman TK <dlshacker159@gmail.com>
2025-11-10 11:52:04 +0530

    100

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -345,25 +345,29 @@ async def cb_handler(client: Client, query: CallbackQuery):
         try:
             files_ = await get_file_details(file_id)
             print(f"[DEBUG] Files retrieved: {files_}")
+            
             if not files_:
                 return await query.answer('No such file exist.', show_alert=True)
-        files = files_[0]
-        title = files.file_name
-        size = get_size(files.file_size)
-        f_caption = files.caption
-        settings = await get_settings(query.message.chat.id)
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
-                logger.exception(e)
-            f_caption = f_caption
-        if f_caption is None:
-            f_caption = f"{files.file_name}"
+            
+            file = files_[0]
+            title = file.file_name
+            size = get_size(file.file_size)
+            f_caption = file.caption
+            settings = await get_settings(query.message.chat.id)
+            
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption = CUSTOM_FILE_CAPTION.format(
+                        file_name='' if title is None else title,
+                        file_size='' if size is None else size,
+                        file_caption='' if f_caption is None else f_caption
+                    )
+                except Exception as e:
+                    logger.exception(e)
+            
+            if f_caption is None:
+                f_caption = f"{title}"
 
-        try:
             if AUTH_CHANNEL and not await is_subscribed(client, query):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
@@ -377,45 +381,58 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
                 )
-                await query.answer(f'Hey {query.from_user.first_name} Check PM, I have sent files in pm(നിൻ്റെ pm നോക്ക് അതിൽ ഞാൻ ഇട്ടിട്ടുണ്ട്)', show_alert=True)
+                await query.answer(f'Hey {query.from_user.first_name} Check PM, I have sent files in pm', show_alert=True)
+                
         except UserIsBlocked:
-            await query.answer('Unblock the bot mahn!(പൊട്ട എന്നെ unblock ചെയ്യൂ)', show_alert=True)
+            await query.answer('Unblock the bot first!', show_alert=True)
         except PeerIdInvalid:
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
         except Exception as e:
+            logger.exception(e)
+            print(f"Error in file handler: {e}")
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
+            
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("I Like Your Smartness(വിളച്ചിൽ എടുകുന്നോ എന്നെ ചൂടാക്കാതെ പോയി സബ് ചെയ്യൂ🙄), But Don't Be Oversmart 😒", show_alert=True)
+            await query.answer("Join the channel first!", show_alert=True)
             return
+        
         ident, file_id = query.data.split("#")
+        
         try:
             files_ = await get_file_details(file_id)
             if not files_:
-                return await query.answer('No such file exist.(അങ്ങനെ ഒരു സാധനവും ഇതിൽ ഇല്ല😑)',show_alert=True)
-        
-        files = files_[0]
-
-        title = files.file_name
-        size = get_size(files.file_size)
-        f_caption = files.caption
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
-                logger.exception(e)
-                f_caption = f_caption
-        if f_caption is None:
-            f_caption = f"{title}"
-        await query.answer()
-        await client.send_cached_media(
-            chat_id=query.from_user.id,
-            file_id=file_id,
-            caption=f_caption,
-            protect_content=True if ident == 'checksubp' else False
-        )
+                return await query.answer('No such file exist.', show_alert=True)
+            
+            file = files_[0]
+            title = file.file_name
+            size = get_size(file.file_size)
+            f_caption = file.caption
+            
+            if CUSTOM_FILE_CAPTION:
+                try:
+                    f_caption = CUSTOM_FILE_CAPTION.format(
+                        file_name='' if title is None else title,
+                        file_size='' if size is None else size,
+                        file_caption='' if f_caption is None else f_caption
+                    )
+                except Exception as e:
+                    logger.exception(e)
+            
+            if f_caption is None:
+                f_caption = f"{title}"
+            
+            await query.answer()
+            await client.send_cached_media(
+                chat_id=query.from_user.id,
+                file_id=file_id,
+                caption=f_caption,
+                protect_content=True if ident == 'checksubp' else False
+            )
+        except Exception as e:
+            logger.exception(e)
+            print(f"Error in checksub: {e}")
+            await query.answer('An error occurred!', show_alert=True)
     elif query.data == "pages":
         await query.answer()
     elif query.data == "start":
