c32006d7a08c203ec2dc19db9f042b14be7baba5
Shibili Aman TK <dlshacker159@gmail.com>
2025-11-10 10:28:21 +0530

    95

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1335,6 +1335,6 @@ async def manual_filters(client, message, text=False):
                         )
                 except Exception as e:
                     logger.exception(e)
-                break
+                return True
     else:
         return False
