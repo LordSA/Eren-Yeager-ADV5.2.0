e89c169f61e32d71647642d0422938d268d355c6
Shibili Aman TK <dlshacker159@gmail.com>
2025-11-10 11:34:24 +0530

    97

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -343,6 +343,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         files_ = await get_file_details(file_id)
         if not files_:
             return await query.answer('No such file exist.')
+        files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
         f_caption = files.caption
@@ -387,6 +388,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         files_ = await get_file_details(file_id)
         if not files_:
             return await query.answer('No such file exist.(അങ്ങനെ ഒരു സാധനവും ഇതിൽ ഇല്ല😑)')
+        files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
         f_caption = files.caption
@@ -1126,6 +1128,7 @@ async def auto_filter(client, msg, spoll=False):
                     return await advantage_spell_chok(msg)
                 else:
                     return
+
         else:
             return
     else:
